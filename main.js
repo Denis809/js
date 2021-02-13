c3304e1cbb52045a4289eddafbeff81b1d8c30ee
Goget <gogetvvv@gmail.com>
2021-02-13 15:31:44 +0200

    created for loop

---
+++ main.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < 10; i++){
+      if ((i % 2) == 1) document.write(i + "<br>");
+  }
+  let sum = 0;
+  for (var i = 0; i <= 10; i++){
+    if ((i % 2) == 1)
+    sum = sum + i;
+  }
+ document.write(sum + " - это сумма чисел");
+     
